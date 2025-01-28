c82c4374ca8cd0e61811aed81c4210f1932bd8b1
StyleNKlick <stylenklick@gmail.com>
2025-01-28 23:52:14 +0530

    Add mongoDB Atlas changes

--- server.js
+++ server.js
@@ -18,14 +18,15 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // app.use(cors());
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
-
+const MONGO_URI = process.env.MONGO_URI || 'your-fallback-connection-string';
 //MongoDb connection
 
-mongoose.connect('mongodb://localhost:27017/stylenklick_db', {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+})
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 // Register a new user
 app.post('/api/register', async (req, res) => {
